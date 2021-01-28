3e9da911ce25f407d50efc67c40d9ee992838ae8
darcyscout <70165647+darcyscout@users.noreply.github.com>
2021-01-27 21:17:16 -0500

    Sausage Dog: I don't know what's wrong
    I can't get it to work even after multiple corrections hmmmm. I think I'm going to try the example code and see what I did wrong

--- exercise1/js/script.js
+++ exercise1/js/script.js
@@ -1,20 +1,51 @@
-/**************************************************
-Template p5 project
-Pippin Barr
+"use strict";
 
-Here is a description of this template p5 project.
-**************************************************/
+const NUM_ANIMAL_IMAGES = 10;
+const NUM_ANIMALS = 100;
 
-// setup()
-//
-// Description of setup() goes here.
-function setup() {
+let animalImages = [];
+let animals = [];
 
+let sausageDogImage = undefined;
+let sausageDog = undefined;
+
+function preload() {
+  for (let i = 0; i < NUM_ANIMAL_IMAGES; i++) {
+    let animalImage = loadImage(`assets/images/animal${i}.png`);
+    animalImages.push(animalImage);
+  }
+
+  sausageDogImage = loadImage(`assets/images/sausage-dog.png`)
 }
 
-// draw()
-//
-// Description of draw() goes here.
-function draw() {
+function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  // Create animalImages
+  for (let i = 0; i < NUM_ANIMALS; i++) {
+    let x = random(0,width);
+    let y = random(0,height);
+    let animalImage = random(animalImages);
+    let animal = new Animal(x, y, animalImage);
+    animals.push(animal);
+  }
+
+  let x = random(0, width);
+  let y = random(0, height);
+  sausageDog = new SausageDog(x, y, sausageDogImage);
+}
+
+
+function draw() {
+  background(255, 255, 0);
+
+  for (let i = 0; i < animals.length; i++) {
+    animals[i].update();
+  }
+
+  sausageDog.update();
+}
+
+function mousePressed() {
+  sausageDog.mousePressed();
 }
