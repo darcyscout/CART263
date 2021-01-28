ebd386bab36fdba0ebaccd57c7fe32290aeb22eb
darcyscout <70165647+darcyscout@users.noreply.github.com>
2021-01-27 21:26:27 -0500

    SausageDog: Found the problem
    I had put the scripts in the wrong order... Pain

--- exercise1/js/SausageDog.js
+++ exercise1/js/SausageDog.js
@@ -1,4 +1,12 @@
+// SausageDog
+// An extension of the Animal class
+// Adds the idea of being found when clicked
+// and spinning when found
+
 class SausageDog extends Animal {
+  // constructor(x,y,image)
+  // Calls the super constructor
+  // Adds properties for being found and for a rotation speed
   constructor(x, y, image) {
     super(x, y, image);
 
@@ -6,20 +14,20 @@ class SausageDog extends Animal {
     this.rotationSpeed = 0.25;
   }
 
+  // update()
+  // Calls the super update() and changes angle if found (to rotate!)
   update() {
     super.update();
-
     if (this.found) {
       this.angle += this.rotationSpeed;
     }
   }
 
+  // mousePressed()
+  // Checks if this sausage dog was clicked and remembers it was found if so
   mousePressed() {
-    if  (mouseX > this.x - this.image.width/2 &&
-        mouseX < this.x + this.image.width/2 &&
-        mouseY > this.y - this.image.height/2 &&
-        mouseY < this.y + this.image.height/2) {
-        this.found = true;
-      }
+    if (!this.found && this.overlap(mouseX, mouseY)) {
+      this.found = true;
     }
   }
+}
