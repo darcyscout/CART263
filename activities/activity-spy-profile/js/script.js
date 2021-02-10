540fc9cd6d94c5eef2b6d6af3b0dc93fc87ac44b
darcyscout <70165647+darcyscout@users.noreply.github.com>
2021-02-10 00:38:27 -0500

    ActivitySpyProfile: Activity complete

--- activities/activity-spy-profile/js/script.js
+++ activities/activity-spy-profile/js/script.js
@@ -16,12 +16,15 @@ let spyProfile = {
 
 let instrumentData = undefined;
 let objectData = undefined;
+let tarotData = undefined;
 
 /**
 Description of preload
 */
 function preload() {
-
+  instrumentData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/music/instruments.json`);
+  objectData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/objects/objects.json`);
+  tarotData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json`);
 }
 
 
@@ -31,10 +34,34 @@ Description of setup
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  spyProfile.name = prompt(`Agent! What is your name?!`);
+  let data = JSON.parse(localStorage.getItem(`spy-profile-data`));
+  if (data !== null) {
+    let password = prompt (`Agent! What is your password`)
+    if (password === data.password) {
+      spyProfile.name = data.name;
+      spyProfile.alias = data.alias;
+      spyProfile.secretWeapon = data.secretWeapon;
+      spyProfile.password = data.password;
+    }
+
+
+
+  }
+  else {
+    generateSpyProfile();
+  }
 }
 
+function generateSpyProfile() {
+  spyProfile.name = prompt(`Agent! What is your name?!`);
+  let instrument = random(instrumentData.instruments);
+  spyProfile.alias = `The ${instrument}`;
+  spyProfile.secretWeapon = random(objectData.objects);
+  let card = random(tarotData.tarot_interpretations);
+  spyProfile.password = random(card.keywords);
 
+  localStorage.setItem(`spy-profile-data`,JSON.stringify(spyProfile));
+}
 /**
 Description of draw()
 */
@@ -55,4 +82,8 @@ Password: ${spyProfile.password}`;
   fill(0);
   text(profile, 100, 100);
   pop();
+}
+
+function mousePressed() {
+  generateSpyProfile();
 }
