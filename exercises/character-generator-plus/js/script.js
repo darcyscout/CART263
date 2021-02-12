5e025fb84f79f508911cac2fcd783d7f581e6ee4
darcyscout <70165647+darcyscout@users.noreply.github.com>
2021-02-11 19:32:26 -0500

    CharacterGeneratorExercise: Complete
    Can generate characteristics and separately type the name for the character. Cool background

--- exercises/character-generator-plus/js/script.js
+++ exercises/character-generator-plus/js/script.js
@@ -4,7 +4,7 @@
 Character Generator
 Darcy Harun w/ the help of Pippin Barr :)
 
-Generates a randomized spy profile for the user, and password protects it.
+Generates characteristics for a named character, typed by the user
 */
 
 let characterProfile = {
@@ -22,6 +22,10 @@ let descriptionsData = undefined;
 let settingData = undefined;
 let tolkienData = undefined;
 let zodiacData = undefined;
+
+let r = 0;
+
+let state = `title`;
 
 /**
 Description of preload
@@ -43,30 +47,26 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
 
-  generateCharacterProfile();
+  let data = JSON.parse(localStorage.getItem(`character-profile-data`));
 
-  // let data = JSON.parse(localStorage.getItem(`character-profile-data`));
-  // if (data !== null) {
-  //   let password = prompt (`Agent! What is your password`)
-  //   if (password === data.password) {
-  //     characterProfile.name = data.name;
-  //     characterProfile.alias = data.alias;
-  //     characterProfile.secretWeapon = data.secretWeapon;
-  //     characterProfile.password = data.password;
-  //     characterProfile.champion = data.champion;
-  //   }
-  //
-  //
-  //
-  // }
-  // else {
-  //   generateCharacterProfile();
-  // }
+  characterProfile.name = data.name;
+  characterProfile.companion = data.companion;
+  characterProfile.setting = data.setting;
+  characterProfile.artifact = data.artifact;
+  characterProfile.sign = data.sign;
+  characterProfile.spirit = data.spirit;
+  characterProfile.attribute = data.attribute;
+
 }
 
 function generateCharacterProfile() {
 
-  characterProfile.name = prompt(`What is your name?`);
+  characterProfile.name = prompt(`What is your character's name?`);
+  localStorage.setItem(`character-profile-data`,JSON.stringify(characterProfile));
+}
+
+function regenerateCharacterProfile() {
+
   characterProfile.companion = random(tolkienData.names);
   let place = random(settingData.settings);
   characterProfile.setting = place.name;
@@ -76,15 +76,7 @@ function generateCharacterProfile() {
   characterProfile.spirit = random(zodiacData.spirit);
   characterProfile.attribute = random(descriptionsData.descriptions);
 
-  // characterProfile.name = prompt(`Agent! What is your name?!`);
-  // let instrument = random(instrumentData.instruments);
-  // characterProfile.alias = `The ${instrument}`;
-  // characterProfile.secretWeapon = random(objectData.objects);
-  // let card = random(tarotData.tarot_interpretations);
-  // characterProfile.password = random(card.keywords);
-  // characterProfile.champion = random(championData.names);
-
-  // localStorage.setItem(`character-profile-data`,JSON.stringify(characterProfile));
+  localStorage.setItem(`character-profile-data`,JSON.stringify(characterProfile));
 }
 
 /**
@@ -93,6 +85,37 @@ Description of draw()
 function draw() {
   background(255);
 
+  if (state === `generate`) {
+    generate();
+  }
+  else if (state === `title`) {
+    displayLines();
+    title();
+  }
+}
+
+function title() {
+  let titleText = `Character
+Generator`;
+
+  push();
+  textFont(`Gothic`);
+  textSize(48);
+  textAlign(CENTER, CENTER);
+  fill(0);
+  text(titleText, width/2, height/2);
+  pop();
+
+  push();
+  textFont(`Gothic`);
+  textSize(18);
+  textAlign(CENTER, CENTER);
+  fill(0);
+  text(`*click to start`, width/2, height/1.1);
+  pop();
+}
+
+function generate() {
   let profile = `未 CHARACTER DESCRIPTION 辰
 
 name ✮ ${characterProfile.name}
@@ -104,12 +127,7 @@ spirit ✣ ${characterProfile.spirit}
 attribute ✹ ${characterProfile.attribute}`;
 
   displayLines();
-
-  push();
-  fill(255);
-  noStroke();
-  ellipse(width/2, height/2, height/2, height/2);
-  pop();
+  displayInstructions();
 
   push();
   textFont(`Gothic, Segoe UI`);
@@ -119,14 +137,32 @@ attribute ✹ ${characterProfile.attribute}`;
   text(profile, width/2, height/2);
   pop();
 
+  if (keyIsPressed === true) {
+    regenerateCharacterProfile();
+  }
+}
+
+function displayInstructions() {
+  let instructionsText = `*click to type a name
+  *press any key to generate characteristics`
+
+  push();
+  textFont(`Gothic`);
+  textSize(18);
+  textAlign(CENTER, CENTER);
+  fill(0);
+  text(instructionsText, width/2, height/1.1);
+  pop();
 }
 
 function displayLines() {
+  r = r + 0.1;
+
   push();
   stroke(0,25);
   strokeWeight(2);
   translate(width/2, height/2);
-  rotate(0);
+  rotate(r);
   line(-width, 0, width, 0);
   pop();
 
@@ -134,7 +170,7 @@ function displayLines() {
   stroke(0,25);
   strokeWeight(2);
   translate(width/2, height/2);
-  rotate(90);
+  rotate(r + 90);
   line(-width, 0, width, 0);
   pop();
 
@@ -142,7 +178,7 @@ function displayLines() {
   stroke(0,25);
   strokeWeight(2);
   translate(width/2, height/2);
-  rotate(45);
+  rotate(r + 45);
   line(-width, 0, width, 0);
   pop();
 
@@ -150,11 +186,23 @@ function displayLines() {
   stroke(0,25);
   strokeWeight(2);
   translate(width/2, height/2);
-  rotate(-45);
+  rotate(r + (-45));
   line(-width, 0, width, 0);
+  pop();
+
+  push();
+  fill(255);
+  noStroke();
+  ellipse(width/2, height/2, height/2, height/2);
   pop();
 }
 
 function mousePressed() {
-  generateCharacterProfile();
+
+  if (state === `title`) {
+    state = `generate`;
+  }
+  else if (state === `generate`) {
+    generateCharacterProfile();
+  }
 }
